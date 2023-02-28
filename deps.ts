4b43f1a28f3e5e9096a0b31d43161c4822f51c3b
RJ <rongjihuang@gmail.com>
2023-02-28 14:37:40 +0800

    Align deps version

--- deps.ts
+++ deps.ts
@@ -8,4 +8,4 @@ export { format as formatBytes } from "https://deno.land/std@0.175.0/fmt/bytes.t
 
 // nextrj
 export { format as formatTemplate, truncateFilename } from "https://deno.land/x/nextrj_utils@0.3.1/string.ts"
-export { format as formatDuration } from "https://deno.land/x/nextrj_utils@0.2.1/duration.ts"
+export { format as formatDuration } from "https://deno.land/x/nextrj_utils@0.3.1/duration.ts"
