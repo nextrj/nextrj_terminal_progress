8f66a82c4a6f75139d3a7dc8a0f8dfb01a2fd275
RJ <rongjihuang@gmail.com>
2023-03-16 15:02:51 +0800

    Upgrade to deno/std@0.180.0

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
 // Copyright 2023 the NextRJ organization. All Rights Reserved. MIT license.
 
 // deno standard library
-export { assertEquals, assertStrictEquals } from "https://deno.land/std@0.175.0/testing/asserts.ts"
-export { delay } from "https://deno.land/std@0.175.0/async/mod.ts"
-export * as colors from "https://deno.land/std@0.175.0/fmt/colors.ts"
-export { format as formatBytes } from "https://deno.land/std@0.175.0/fmt/bytes.ts"
+export { assertEquals, assertStrictEquals } from "https://deno.land/std@0.180.0/testing/asserts.ts"
+export { delay } from "https://deno.land/std@0.180.0/async/mod.ts"
+export * as colors from "https://deno.land/std@0.180.0/fmt/colors.ts"
+export { format as formatBytes } from "https://deno.land/std@0.180.0/fmt/bytes.ts"
 
 // nextrj
 export { format as formatTemplate, truncateFilename } from "https://deno.land/x/nextrj_utils@0.3.1/string.ts"
